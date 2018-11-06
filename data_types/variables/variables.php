d22ae2cf481673554a5e36f36523f33073756260
sojurner <pykim0591@gmail.com>
2018-11-06 08:38:26 -0700

    Move previous files to respective folders

---
+++ data_types/variables/variables.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <meta http-equiv="X-UA-Compatible" content="ie=edge">
+  <title>Document</title>
+</head>
+<body>
+  <?php 
+
+  $name = 'Paul';
+    $number = 10120;
+    $number_List = 100;
+    $Name = "<h1>paul</h1>";
+    echo $name . " " . $Name;
+  ?>
+
+
+
+</body>
+</html>
