2c0967a633a9963d2caa4238ec443c3959f0ab11
sojurner <pykim0591@gmail.com>
2018-11-07 15:33:38 -0700

    Add math file

---
+++ functions/built-in_functions/math.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html lang='en'>
+<head>
+<link rel='stylesheet' href='https://use.fontawesome.com/releases/v5.0.13/css/all.css' integrity='sha384-DNOHZ68U8hZfKXOrtjWvjxusGo9WQnrNx2sqG0tfsghAvtVlRW3tvkXWZh58N9jp' crossorigin='anonymous'>
+<meta name='viewport' content='width=device-width, initial-scale=1.0'>
+<link rel='stylesheet' href='normalize.css'>
+<link rel='stylesheet' type='text/css' href='style.css'>
+<title>Static Composition 3</title>
+</head>
+<body>
+<?php 
+// php.net/manual/en/ref.math.php
+echo pow(2,7);
+
+echo "<br>";
+
+echo rand(); //echo rand(1, 10)
+
+echo "<br>";
+
+echo sqrt(100);
+
+echo "<br>";
+
+echo ceil(4.6);
+
+echo "<br>";
+
+echo floor(4.5);
+?>
+</body>
+</html>
