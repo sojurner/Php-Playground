b6700400992d38e9c97d1799829588c2fb028520
sojurner <pykim0591@gmail.com>
2018-11-09 10:38:40 -0700

    Add form_external.php file

---
+++ form_data/form_external.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html lang='en'>
+<head>
+<meta name='viewport' content='width=device-width, initial-scale=1.0'>
+<link rel='stylesheet' href='normalize.css'>
+<link rel='stylesheet' type='text/css' href='style.css'>
+<title>Static Composition 3</title>
+</head>
+<body>
+
+<form action="form_process.php" method="post">
+
+    <input type="username" name="username" placeholder="username">
+    <input type="password" name="password" placeholder="password">
+    <button type="submit" name="submit">submit</button>
+
+</form>
+
+<!-- <form action="form.php" method="post">
+
+<input type="text" name="username" placeholder="enter username"/>
+<input type="password" name="password" placeholder="enter password"/>
+<input type="submit" placeholder="submit" name="submit"/> -->
+
+<!-- </form> -->
+
+
+</body>
+</html>
