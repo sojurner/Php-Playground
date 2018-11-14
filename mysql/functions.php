002897cf0c5f8d923b076bb6c098300a23c975b4
sojurner <pykim0591@gmail.com>
2018-11-14 16:16:53 -0700

    Create Login function

--- mysql/functions.php
+++ mysql/functions.php
@@ -1,4 +1,17 @@
-<?php include "db.php";
+<?php include "db.php"?>
+<?php  
+
+function Login(){
+global $connection; //ensure connection is global since connections is within the functions scope
+
+$username = $_POST['username'];
+$password = $_POST['password'];
+
+if($connection){
+  echo "connected";
+} else {
+  die("Connection failed");
+}
 
 function showAllData() {
   global $connection; //ensure connection is global since connections is within the functions scope
